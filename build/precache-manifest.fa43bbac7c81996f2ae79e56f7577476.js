eb15a9c3c28a49627b4925eb8c3ae527989e5208
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-12-11 15:01:07 +0100

    production build

---
+++ build/precache-manifest.fa43bbac7c81996f2ae79e56f7577476.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1f719c29f52c58ee705ddf389c24a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b622cc8ec505cb402fe7",
+    "url": "/static/css/main.32f80920.chunk.css"
+  },
+  {
+    "revision": "de1ab3b06be77bee0cc2",
+    "url": "/static/js/2.bcee8d3a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bcee8d3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b622cc8ec505cb402fe7",
+    "url": "/static/js/main.61659a03.chunk.js"
+  },
+  {
+    "revision": "03dd0e16aea3b64bc1c3",
+    "url": "/static/js/runtime-main.43b4615e.js"
+  },
+  {
+    "revision": "84274072c4f64eada90ce975099d3fa4",
+    "url": "/static/media/cover-photo-2.84274072.jpg"
+  },
+  {
+    "revision": "e7621c7553847ec2907ecd35088b6561",
+    "url": "/static/media/noroff-logo.e7621c75.png"
+  }
+]);
