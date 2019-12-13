ee164ac0f66e5aede29fb0d1778fc898135279e5
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-12-13 14:49:56 +0100

    paths

--- build/precache-manifest.6f7fe1c90fe0862b2d89b72a309a2cc3.js
+++ build/precache-manifest.6f7fe1c90fe0862b2d89b72a309a2cc3.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "01c8e0378688fbfc7bab8ab753906af4",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "2140edc8c86f9cc2f09e",
-    "url": "/static/css/main.24a02a0d.chunk.css"
+    "url": "./static/css/main.24a02a0d.chunk.css"
   },
   {
     "revision": "de1ab3b06be77bee0cc2",
-    "url": "/static/js/2.bcee8d3a.chunk.js"
+    "url": "./static/js/2.bcee8d3a.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/static/js/2.bcee8d3a.chunk.js.LICENSE"
+    "url": "./static/js/2.bcee8d3a.chunk.js.LICENSE"
   },
   {
     "revision": "2140edc8c86f9cc2f09e",
-    "url": "/static/js/main.99953d15.chunk.js"
+    "url": "./static/js/main.99953d15.chunk.js"
   },
   {
     "revision": "03dd0e16aea3b64bc1c3",
-    "url": "/static/js/runtime-main.43b4615e.js"
+    "url": "./static/js/runtime-main.43b4615e.js"
   },
   {
     "revision": "58ed526c11e45cba0f39047dbb471f23",
-    "url": "/static/media/cover-photo-2.58ed526c.jpg"
+    "url": "./static/media/cover-photo-2.58ed526c.jpg"
   },
   {
     "revision": "e7621c7553847ec2907ecd35088b6561",
-    "url": "/static/media/noroff-logo.e7621c75.png"
+    "url": "./static/media/noroff-logo.e7621c75.png"
   }
 ]);
